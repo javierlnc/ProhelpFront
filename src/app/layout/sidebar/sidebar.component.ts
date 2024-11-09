7d17c9a3ca1bfdd0d191364c1c00c195e8921735
javierlnc <javierlnc@icloud.com>
2024-11-09 16:23:57 -0500

    Se realizan los cambios para rol

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,7 +1,14 @@
-import { trigger, state, style, transition, animate } from '@angular/animations';
-import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '@services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,26 +16,37 @@ import { RouterLink } from '@angular/router';
   imports: [CommonModule, RouterLink],
   animations: [
     trigger('sidebarAnimation', [
-      state('open', style({
-        transform: 'translateX(0px)',
-        opacity: 1
-      })),
-      state('closed', style({
-        transform: 'translateX()', // Ajustar según tu diseño
-        opacity: 1
-      })),
-      transition('open <=> closed', [
-        animate('500ms')
-      ])
-    ])
+      state(
+        'open',
+        style({
+          transform: 'translateX(0px)',
+          opacity: 1,
+        })
+      ),
+      state(
+        'closed',
+        style({
+          transform: 'translateX()', // Ajustar según tu diseño
+          opacity: 1,
+        })
+      ),
+      transition('open <=> closed', [animate('500ms')]),
+    ]),
   ],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  rol!: any;
+  constructor(private authService: AuthService) {}
+  ngOnInit(): void {
+    this.getRole();
+  }
   sidebarState: 'open' | 'closed' = 'closed';
   toggleSidebar() {
     this.sidebarState = this.sidebarState === 'closed' ? 'open' : 'closed';
   }
-
+  getRole() {
+    this.rol = this.authService.getUser()?.rol;
+  }
 }
